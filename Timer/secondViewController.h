c2f54005c2064344bd8352643108f58e42f71bc0
alancodvo <alancodvo@gmail.com>
2015-01-21 15:19:07 +0900

    secondViewControllerクラスを追加

---
+++ Timer/secondViewController.h
@@ -0,0 +1,13 @@
+//
+//  secondViewController.h
+//  Timer
+//
+//  Created by alan-arakawa-yoshihiro on H27/01/21.
+//  Copyright (c) 平成27年 Yoshihiro Arakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface secondViewController : UIViewController
+
+@end
