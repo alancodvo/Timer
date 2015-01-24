6fc5ce6c6680cf49c7e421d03360ed8730d56f35
alancodvo <alancodvo@gmail.com>
2015-01-24 10:33:44 +0900

    コピペ時にできてた空白の削除

--- Timer/ViewController.m
+++ Timer/ViewController.m
@@ -23,28 +23,18 @@
 @interface ViewController () <UIAlertViewDelegate>{
     
     // カウント変数を用意する
-    
     int count;
-    
     NSString *selectTimeHour;
-    
     NSString *selectTimeMinute;
     
 }
 
 @property (weak, nonatomic) IBOutlet UILabel *indexLabel;
-
 @property (weak, nonatomic) IBOutlet UIDatePicker *myDatePicker;
-
 - (IBAction)changeDatePicker:(id)sender;
-
 - (IBAction)tapBtn:(id)sender;
-
 - (IBAction)returnMain:(UIStoryboardSegue *)segue;
-
 @property (nonatomic, assign) NSString *selectedTime;
-
-
 
 @end
 
@@ -63,11 +53,8 @@
     
     
     // 変数の初期化
-    
     count = 0;
-    
     selectTimeHour = @"";
-    
     selectTimeMinute = @"";
     
 }
@@ -89,11 +76,8 @@
 - (IBAction)changeDatePicker:(id)sender {
     
     NSDateFormatter *df = [[NSDateFormatter alloc] init];
-    
     [df setDateFormat:@"HH:mm"];
-    
     NSString *datestr = [df stringFromDate:self.myDatePicker.date];
-    
     NSLog(@"%@", datestr);
     
 }
@@ -105,31 +89,20 @@
 - (IBAction)tapBtn:(id)sender {
     
     // 選択時刻のフォーマット指定とログ出力
-    
     NSDateFormatter *df2 = [[NSDateFormatter alloc] init];
-    
     [df2 setDateFormat:@"HH:mm"];
-    
     NSString *datestr2 = [df2 stringFromDate:self.myDatePicker.date];
-    
     NSLog(@"%@", datestr2);
     
     
     
     // アラートの設定
-    
     UIAlertView *alert = [[UIAlertView alloc] init];
-    
     alert.title = @"起床時刻を設定しました。";
-    
     alert.message = datestr2;
-    
     alert.delegate = self;
-    
     [alert addButtonWithTitle:@"キャンセル"];
-    
     [alert addButtonWithTitle:@"OK"];
-    
     [alert show];
     
 }
@@ -141,17 +114,13 @@
 - (IBAction)returnMain:(UIStoryboardSegue *)segue {
     
     // 前の画面から値を受け取って
-    
     secondViewController *newVC = [segue sourceViewController];
-    
     count = newVC.myCount;
     
     // カウントアップして
-    
     count++;
     
     // 表示する
-    
     NSLog(@"カウンター<%d>", count);
     
 }
@@ -161,41 +130,27 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     
     NSDateFormatter *dfHour = [[NSDateFormatter alloc] init];
-    
     NSDateFormatter *dfMinute = [[NSDateFormatter alloc] init];
     
     [dfHour setDateFormat:@"HH"];
-    
     [dfMinute setDateFormat:@"mm"];
     
     NSString *datestrHour = [dfHour stringFromDate:self.myDatePicker.date];
-    
     NSString *datestrMinute = [dfMinute stringFromDate:self.myDatePicker.date];
     
     selectTimeHour = datestrHour;
-    
     selectTimeMinute = datestrMinute;
     
     // もしセグエの名前がmySegueだったら
-    
     if ( [[segue identifier] isEqualToString:@"mySegue"]) {
-        
         // カウントアップして
-        
         count++;
-        
         // 次の画面へ値を受け渡す
-        
         secondViewController * newVC = [segue destinationViewController];
-        
         newVC.myCount = count;
-        
         newVC.selectedTimeHour = selectTimeHour;
-        
         newVC.selectedTimeMinute = selectTimeMinute;
-        
     }
-    
 }
 
 
